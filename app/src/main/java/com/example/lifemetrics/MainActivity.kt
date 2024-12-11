5cc0aeb0c96c778710af91e0a15d76ae0cf20189
Vanessa <147354806+MarNess24@users.noreply.github.com>
2024-12-10 22:34:51 -0600

    Frontend de RegistrosView terminada (#1)

--- app/src/main/java/com/example/lifemetrics/MainActivity.kt
+++ app/src/main/java/com/example/lifemetrics/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lifemetrics
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import com.example.lifemetrics.navigation.NavManager
 import com.example.lifemetrics.ui.theme.LifeMetricsTheme
--- app/src/main/java/com/example/lifemetrics/views/RegistrosView.kt
+++ app/src/main/java/com/example/lifemetrics/views/RegistrosView.kt
@@ -1,70 +1,131 @@
 package com.example.lifemetrics.views
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lifemetrics.R
-import com.example.navigateprojects.components.ActionButton
+import com.example.navigateprojects.components.PersonCard
+import com.example.navigateprojects.components.SpaceH
 import com.example.navigateprojects.components.TitleBar
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun RegistrosView() {
-    /*Column (
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFFFFF))
     ) {
+        // Imagen de fondo
+        Image (
+            painter = painterResource ( id = R.drawable.item2 ),
+            contentDescription = "Detalle izquierdo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomStart)
+        )
 
-    }*/
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                modifier = Modifier
-                    //.statusBarsPadding()
-                    .height(150.dp),
-                title = {
-                    Image(
-                            painter = painterResource(id = R.drawable.logo), // Cambia "logo" por el nombre de tu recurso de imagen.
-                            contentDescription = "Logo",
-                            modifier = Modifier.width(100.dp) // Ajusta el tamaño del logo.
-                        )
-                    Spacer(modifier = Modifier.width(20.dp)) // Espacio entre el logo y el texto.
-                    TitleBar(name = "Control Diabetes")
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors (
-                    containerColor = Color ( 0xFF6481C1 )
-                )
+        Column (
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // Logo y "Control Diabetes"
+            TitleBar (
+                name = "Control Diabetes",
+                textColor = Color ( 0xFF49688D ),
+                backgroundColor = Color ( 0xFFB0C1D9 ),
+                image = R.drawable.logo
             )
-        },
-        floatingActionButton = {
-            ActionButton()
+
+            SpaceH ( 20.dp)
+
+            // Título de los registros "Personas Registradas"
+            Text (
+                text = "Personas registradas",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color ( 0xFF49688D ),
+                modifier = Modifier
+                    .padding ( start = 16.dp, top = 16.dp )
+            )
+
+            SpaceH ( 20.dp )
+
+            // Usuarios registrados desde la cuenta actual
+            Registros()
         }
-    ) {
-        ContentRegistrosView()
+
+        // Botón "Agregar"
+        FloatingActionButton (
+            onClick = { /* Códigou */ },
+            containerColor = Color ( 0xFF6481C1 ),
+            contentColor = Color.White,
+            shape = CircleShape,
+            modifier = Modifier
+                .size ( 80.dp )
+                .align ( Alignment.BottomEnd )
+                .offset ( y = -85.dp, x = -40.dp )
+        ) {
+            Icon (
+                imageVector = Icons.Default.Add,
+                contentDescription = "Agregar",
+                modifier = Modifier.size ( 50.dp )
+            )
+        }
+
+        // Texto "Agregar"
+        Text (
+            text = "Agregar",
+            fontSize = 20.sp,
+            color = Color ( 0xFF49688D ),
+            modifier = Modifier
+                .align ( Alignment.BottomEnd )
+                .padding ( end = 43.dp, bottom = 170.dp )
+        )
     }
 }
 
 @Composable
-fun ContentRegistrosView() {
-    /**/
+fun Registros() {
+    // Registros del usuario actual
+    val people = listOf ( "Menganito", "María", "Luna", "Mariana", "Scar", "Karla", "Secretaria" )
+
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding ( horizontal = 16.dp )
+            .height ( 375.dp )
+    ) {
+        LazyColumn (
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy ( 8.dp )
+        ) {
+            items ( people ) { name ->
+                PersonCard ( name = name )
+            }
+        }
+    }
 }
